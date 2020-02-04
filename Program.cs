b5add49ab2b785a2db9f9371522e6a34a89dac5b
Meri <twinkling.star.m.t@gmail.com>
2020-02-04 22:19:21 +0900

    Init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp_Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
